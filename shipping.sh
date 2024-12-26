7d6606298fe944b08be7d8bb43c9942d42f15bcb
Dennis Guerra <dennisguerra1@gmail.com>
2024-12-25 18:58:44 -0600

    Roboshop Project update from class

---
+++ shipping.sh
@@ -0,0 +1,17 @@
+source ./common.sh
+app_name=shipping
+
+JAVA
+
+echo Install MySQL
+dnf install mysql -y &>>$log_file
+Status_Print $?
+
+
+for file in schema app-user master-data; do
+
+  echo Load $file
+  mysql -h mysql-dev.azdevopsb82.online -uroot -pRoboShop@1 < /app/db/$file.sql &>>$log_file
+  Status_Print $?
+
+done
